3d2a9aa072879e113b154c48d338b4060415e4f4
Michael Eaton <mikejeaton@hotmail.com>
2020-01-14 16:28:35 +0000

    Add basic unit tests for EnigmaMachine interface.

---
+++ DotNetEnigma/DotNetEnigmaTests/Unit Tests/EnigmaMachineTests.cs
@@ -0,0 +1,43 @@
+﻿using DotNetEnigma.Enigma;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetEnigmaTests.Unit_Tests
+{
+    [TestFixture]
+    public class EnigmaMachineTests
+    {
+        [Test]
+        public void EnigmaMachine_InitializedWithStaticFactory_IsNotNull()
+        {
+            var enigmaMachine = EnigmaMachine.Default();
+
+            enigmaMachine.Should()
+                .NotBeNull();
+        }
+
+        [Test]
+        public void DefaultEnigmaMachine_EncryptText_DifferentTextReturned()
+        {
+            var textToEncrypt = "Text Information.";
+            var enigmaMachine = EnigmaMachine.Default();
+
+            var result = enigmaMachine.Encrypt(textToEncrypt);
+
+            result.Should()
+                .NotBe(textToEncrypt);
+        }
+
+        [Test]
+        public void DefaultEnigmaMachine_DecryptText_DifferentTextReturned()
+        {
+            var textToDecrypt = "Text Information.";
+            var enigmaMachine = EnigmaMachine.Default();
+
+            var result = enigmaMachine.Decrypt(textToDecrypt);
+
+            result.Should()
+                .NotBe(textToDecrypt);
+        }
+    }
+}
