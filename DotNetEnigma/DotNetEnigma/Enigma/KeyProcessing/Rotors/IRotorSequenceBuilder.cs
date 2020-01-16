6dd53e72886174cbdc6389d0853bdedbdbefc697
Michael Eaton <mikejeaton@hotmail.com>
2020-01-16 19:10:04 +0000

    Add RotorSequenceBuilder.

---
+++ DotNetEnigma/DotNetEnigma/Enigma/KeyProcessing/Rotors/IRotorSequenceBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetEnigma.Enigma.KeyProcessing.Rotors
+{
+    interface IRotorSequenceBuilder
+    {
+        void Reset();
+        void BuildReflector();
+        void BuildRotor(RotorNumber number, int notchPosition);
+
+    }
+}
---
+++ DotNetEnigma/DotNetEnigma/Enigma/KeyProcessing/Rotors/RotorSequenceBuilder.cs
@@ -0,0 +1,39 @@
+﻿using DotNetEnigma.Configuration;
+using System.Linq;
+
+namespace DotNetEnigma.Enigma.KeyProcessing.Rotors
+{
+    public class RotorSequenceBuilder : IRotorSequenceBuilder
+    {
+        private RotorSequence _instance = new RotorSequence();
+
+        public void BuildReflector()
+        {
+            _instance.Reflector = new Reflector();
+        }
+
+        public void BuildRotor(RotorNumber number, int notchPosition)
+        {
+            var configuration = DotNetEnigmaConfiguration.RotorWiringConfiguration
+                .EnigmaMachines.FirstOrDefault()
+                    .WiringConfigurations.FirstOrDefault(x => x.Rotor == number);
+            var newRotor = new Rotor(notchPosition, configuration.Rotor, configuration.Configuration);
+
+            _instance.RotorSeries.Enqueue(newRotor);
+        }
+
+        public void Reset()
+        {
+            _instance = new RotorSequence();
+        }
+
+        public RotorSequence Assemble()
+        {
+            var instanceToReturn = _instance;
+
+            Reset();
+
+            return instanceToReturn;
+        }
+    }
+}
